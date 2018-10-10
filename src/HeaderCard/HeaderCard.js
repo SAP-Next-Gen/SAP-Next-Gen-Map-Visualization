edb31805baca42070025c6994faa0b5abeee57e1
jonashering <jnshering@gmail.com>
2018-10-10 16:11:54 -0400

    Updtae Headercard

--- src/HeaderCard/HeaderCard.js
+++ src/HeaderCard/HeaderCard.js
@@ -5,9 +5,9 @@ import XLSX from 'xlsx';
 class HeaderCard extends Component {
     constructor(props) {
         super(props);
-        this.setState({
+        this.state = {
             entries: [],
-        })
+        };
     }
 
     handleInputFileChange(files) {
@@ -26,24 +26,23 @@ class HeaderCard extends Component {
         let workbook = XLSX.read(table, {type: 'array'});
         let first_worksheet = workbook.Sheets[workbook.SheetNames[0]];
         let data = XLSX.utils.sheet_to_json(first_worksheet, {range: 4});
-        data = data.slice(0, 4)
-        data = data.map(async entry => {
-            console.log(entry);
-            console.log(this.fetchLocation(entry.City));
-            return entry.coordinates = this.fetchLocation(entry.City);
+        data.forEach(entry => {
+            this.fetchLocation(entry);
         });
-        let self = this;
-        Promise.all(data).then(data => self.setState({entries: data}))
-        console.log(data);
     }
 
-    fetchLocation(city) {
-        fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + city + ".json?access_token=" + process.env.REACT_APP_MAPBOX_KEY)
+    fetchLocation(entry) {
+        fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + entry.City + ".json?access_token=" + process.env.REACT_APP_MAPBOX_KEY)
             .then(data => data.json())
             .then(data => {
-                console.log(data.features[0].geometry.coordinates)
-                return data.features[0].geometry.coordinates;
-            });
+                entry.coordinates = data.features[0].geometry.coordinates;
+                this.setState((state) => {
+                    return {entries: state.entries.concat(entry)};
+                });
+            })
+            .catch(e => {
+                console.log("Couldn't find location for " + entry.Name + " in " + entry.City)
+            })
     }
 
     render() { 
